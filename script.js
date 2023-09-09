717569392687bcb84386a228bb1ad4597cae99ca
mukeshrai1997 <135120496+mukeshrai1997@users.noreply.github.com>
2023-09-09 20:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function firstNonRepeatedChar(str) {
+
  // Write your code here
+	function firstNonRepeatedChar(s) {
+    // Create a Map to store the frequency of each character
+    const charCount = new Map();
+
+    // Iterate through the string to count the frequency of each character
+    for (const char of s) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    // Iterate through the string again to find the first non-repeated character
+    for (const char of s) {
+        if (charCount.get(char) === 1) {
+            return char;
+        }
+    }
+
+    // If no non-repeated character is found, return null
+    return null;
+}
+
+// Test cases
+console.log(firstNonRepeatedChar('aabbcdd'));  // Output: 'c'
+console.log(firstNonRepeatedChar('aabbcc'));   // Output: null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
